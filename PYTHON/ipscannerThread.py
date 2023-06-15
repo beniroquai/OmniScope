bf71a05fb2a15d4083a819852c2f7ba738d67bd2
beniroquai <bene.d@gmx.de>
2023-06-15 20:17:00 +0200

    Adding latest CAD Drawings

--- PYTHON/ipscannerThread.py
+++ PYTHON/ipscannerThread.py
@@ -2,33 +2,33 @@ import requests
 import numpy as np
 import threading
 import time
+import json
 
 allScopes = []
 
 # Define a function for the thread
-def findOmniscope(subnet="", ipRange=(0,50),endpoint="/getID"):
-    endpoint="/getid"
+def findOmniscope(subnet="", ipRange=(0,50),endpoint="/status"):
     print("searching in range: "+str(ipRange))
     for ip in range(*ipRange):
        URL = subnet+str(ip)+endpoint
        #print(URL)
        try:
-           source = requests.get(URL,timeout=0.5)
-           if(str(source.content).find("OMNISCOPE")>0):
-               allScopes.append(subnet+str(ip))
+            source = requests.get(URL,timeout=0.5)
+            omniscopeID = source.json()['omniscope']
+            omniscopeIP = source.json()['stream_url'].split(":8")[0]
+            allScopes.append({"IP":omniscopeIP,
+                              "ID":omniscopeID})
        except :
            pass
        
 def findOmniscopes(subnet, nThreads = 20):
     findScopeThreads = []
     for iThread in range(np.int32(np.ceil(255/nThreads))):
-        findScopeThreads.append(threading.Thread(target=findOmniscope, args=(subnet, (nThreads*iThread,iThread*nThreads+nThreads), "/getID")))
+        findScopeThreads.append(threading.Thread(target=findOmniscope, args=(subnet, (nThreads*iThread,iThread*nThreads+nThreads), "/status")))
         findScopeThreads[-1].start()
-        
     print("Waiting to finish the search in "+str(nThreads)+" Threads")
     while(True):
         isAlive = True
-    
         for iThread in findScopeThreads:
             isAlive &= iThread.is_alive()
         if not isAlive:
@@ -38,7 +38,13 @@ def findOmniscopes(subnet, nThreads = 20):
 
     return allScopes
         
+
+baseURL = "http://192.168.43."
         
-allOmniscopes = findOmniscopes("http://192.168.2.",nThreads=40)
+allOmniscopes = findOmniscopes(baseURL, nThreads=40)
 
 print(allOmniscopes)
+
+
+
+# %%
